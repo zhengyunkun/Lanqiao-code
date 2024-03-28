9e87e17565912e08be6c6b9348d92c82172fbec7
ZhengYunkun <2240324227@qq.com>
2024-03-28 15:18:41 +0800

    nothing

--- exercises/Acwing3494-1.cpp
+++ exercises/Acwing3494-1.cpp
@@ -10,19 +10,24 @@ LL res;
 int m, n, k;
 int g[10][110];
 
-int dx[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
-int dy[8] = {1, 2, 2, 1, -1, -2, -2, -1};
-int dx2[4] = {-2, -1, 1, 2}, dy2[4] = {1, 2, 2, 1};
+int dx2[4] = {-2, -1, 1, 2}; int dy2[4] = {1, 2, 2, 1}; // 只考虑上面的🐎，下面的下面的下面会考虑
 
-string filename = "ans.txt";
+string filename = "ans-1.txt";
 ofstream out(filename);
 
-void dfs1(int x, int y, int cur)
+void dfs2(int x, int y, int cur)
 {
     if(cur == k) 
     {
         res ++;
         res = res % MOD;
+        out << "case " << res << ":\n";
+        for (int i = 0; i < n; i ++ )
+        {
+            for (int j = 0; j < n; j ++ )
+                out << g[i][j] << " ";
+            out << endl;
+        }
         return ;
     }
 
@@ -30,12 +35,12 @@ void dfs1(int x, int y, int cur)
     if(x > n) return ;
 
 
-    dfs1(x, y + 1, cur);
+    dfs2(x, y + 1, cur);
 
     int flag = 1;
-    for(int j = 0; j < 8; j ++) // 检查和之前的🐎是否冲突
+    for(int j = 0; j < 4; j ++) // 检查和之前的🐎是否冲突
     {
-        int a = x + dx[j], b = y + dy[j];
+        int a = x + dx2[j], b = y + dy2[j];
         if(a >= 0 && a <= n && b >= 0 && b <= m)
             if(g[a][b])
             {
@@ -47,8 +52,8 @@ void dfs1(int x, int y, int cur)
     if(flag) 
     {
         g[x][y] = 1;
-        dfs1(x, y + 1, cur + 1);
-        g[x][y] = 0;   
+        dfs2(x, y + 1, cur + 1);
+        g[x][y] = 0;    
     }
 }
 
@@ -56,7 +61,7 @@ int main()
 {
     cin >> n >> m >> k;
 
-    dfs1(1, 1, 0);
+    dfs2(1, 1, 0);
 
     cout << res;
 }
